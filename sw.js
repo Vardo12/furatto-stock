ab391017a1f863804f0e9a415910b15dc5cf3b3b
Vardo12 <jordan40becerra@gmail.com>
2025-10-03 11:51:14 -0300

    Fix: Configurar para funcionamiento offline

--- sw.js
+++ sw.js
@@ -1,17 +1,20 @@
-const CACHE_NAME = 'furatto-stock-v1';
+const CACHE_NAME = 'furatto-stock-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './app.js', 
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
@@ -19,11 +22,11 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // Devuelve el cache si existe, sino haz fetch
         if (response) {
           return response;
         }
         return fetch(event.request);
-      }
-    )
+      })
   );
 });
